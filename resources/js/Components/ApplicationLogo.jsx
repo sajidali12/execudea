8646a79ac64edef9143562a79775cdcca0951cd5
sajidali12 <rizvisajid4@gmail.com>
2024-03-11 17:39:21 +0500

    logo update

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -2,7 +2,7 @@ export default function ApplicationLogo(props) {
  
     return (
         <div {...props}>
-        <img src='/img/execuda.png' />
+        <img src='/img/execudea.png' />
         </div>
     );
 }
